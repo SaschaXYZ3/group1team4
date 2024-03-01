c77e552b7a6dd87674bb64981f26ac0bddaab02c
Sascha Hartl <sascha.hartl@stud.fh-campuswien.ac.at>
2024-03-01 23:34:19 +0100

    add basic

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/Genre.java
@@ -0,0 +1,23 @@
+package at.ac.fhcampuswien.fhmdb;
+public enum Genre {
+    ACTION,
+    ADVENTURE,
+    ANIMATION,
+    BIOGRAPHY,
+    COMEDY,
+    CRIME,
+    DRAMA,
+    DOCUMENTARY,
+    FAMILY,
+    FANTASY,
+    HISTORY,
+    HORROR,
+    MUSICAL,
+    MYSTERY,
+    ROMANCE,
+    SCIENCE_FICTION,
+    SPORT,
+    THRILLER,
+    WAR,
+    WESTERN
+}
